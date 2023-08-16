b717648d4d56709c1b922b3429090cff097f87d7
EcaCosca <enriquecoscarelli@gmail.com>
2023-08-16 09:44:43 +0200

    Using route

--- app.js
+++ app.js
@@ -4,28 +4,40 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
 
-app.get('/', (req, res) => {
-    console.log(req.method);
-  res.send('GET REQUEST')
+// app.get('/', (req, res) => {
+//     console.log(req.method);
+//   res.send('GET REQUEST')
+// })
+
+// app.post('/', (req, res) => {
+//     console.log(req.method);
+//   res.send('POST REQUEST')
+// })
+
+// app.put('/', (req, res) => {
+//     console.log(req.method);
+//   res.send('PUT REQUEST')
+// })
+
+// app.delete('/', (req, res) => {
+//     console.log(req.method);
+//   res.send('DELETE REQUEST')
+// })
+
+// app.use()
+
+app.route('/')
+.get((req, res) => {res.send('GET REQUEST')})
+.post((req, res) => {res.send('POST REQUEST')})
+.put((req, res) => {res.send('PUT REQUEST')})
+.delete((req, res) => {res.send('DELETE REQUEST')})
+
+app.get('/users', (req, res) => {
+  res.send('Here you get all the users')
 })
 
-app.post('/', (req, res) => {
-    console.log(req.method);
-  res.send('POST REQUEST')
-})
-
-app.put('/', (req, res) => {
-    console.log(req.method);
-  res.send('PUT REQUEST')
-})
-
-app.delete('/', (req, res) => {
-    console.log(req.method);
-  res.send('DELETE REQUEST')
-})
-
-app.get('/stephan', (req, res) => {
-  res.send('Hello Stephan!')
+app.get('/users/id', (req, res) => {
+  res.send('A single user')
 })
 
 app.get('/christoph', (req, res) => {
