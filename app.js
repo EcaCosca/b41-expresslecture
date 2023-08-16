d86a01f3c1814e0e9094c507e2c891faf8c6b002
EcaCosca <enriquecoscarelli@gmail.com>
2023-08-16 10:41:49 +0200

    pug

--- app.js
+++ app.js
@@ -3,42 +3,42 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
-
-// app.get('/', (req, res) => {
-//     console.log(req.method);
-//   res.send('GET REQUEST')
-// })
-
-// app.post('/', (req, res) => {
-//     console.log(req.method);
-//   res.send('POST REQUEST')
-// })
-
-// app.put('/', (req, res) => {
-//     console.log(req.method);
-//   res.send('PUT REQUEST')
-// })
-
-// app.delete('/', (req, res) => {
-//     console.log(req.method);
-//   res.send('DELETE REQUEST')
-// })
+const users = require('./users')
 
 // app.use()
+app.set('view engine', 'pug')
 
+// ALL REQUESTS TO ENDPOINT "/"
 app.route('/')
+.all((req, res, next) => {
+    console.log("METHOD: "+req.method)
+    next()
+})
 .get((req, res) => {res.send('GET REQUEST')})
 .post((req, res) => {res.send('POST REQUEST')})
 .put((req, res) => {res.send('PUT REQUEST')})
 .delete((req, res) => {res.send('DELETE REQUEST')})
 
+// GET - ALL USERS
 app.get('/users', (req, res) => {
-  res.send('Here you get all the users')
+    res.json(users)
 })
 
-app.get('/users/id', (req, res) => {
-  res.send('A single user')
+// GET - SINGLE USER BY ID
+app.get('/users/:id', (req, res) => {
+    // HERE ARE THE REQ PARAMETERS BEING STORED
+    console.log(req.params.id);
+    // HERE ARE THE REQ QUERYS BEING STORED
+    console.log(req.query.name);
+    const result = users.filter(ele => ele.id == req.params.id)
+    res.json(result)
 })
+
+// GET - PUG
+app.get('/pug', (req, res) => {
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
+
 
 app.get('/christoph', (req, res) => {
   res.send('Hello Christoph!')
---
+++ users.js
@@ -0,0 +1,22 @@
+const users = [
+    {
+        id: 1,
+        name: "Christoph",
+        age: 25,
+        email: "c.loveToCode@wbs.com"
+    },
+    {
+        id: 2,
+        name: "Oleksiy",
+        age: 25,
+        email: "o.loveToCode@wbs.com"
+    },
+    {
+        id: 3,
+        name: "Stephan",
+        age: 25,
+        email: "s.loveToCode@wbs.com"
+    },
+]
+
+module.exports = users
