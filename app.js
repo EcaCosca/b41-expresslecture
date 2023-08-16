24b2c2807e317a9a31c339455a00589ccaacdb2d
EcaCosca <enriquecoscarelli@gmail.com>
2023-08-16 09:22:38 +0200

    dotenv applied

---
+++ app.js
@@ -0,0 +1,13 @@
+// https://www.npmjs.com/package/dotenv
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}/`)
+})
