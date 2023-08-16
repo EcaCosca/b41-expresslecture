849812f2982a4562882ceeb0559394a91106755d
EcaCosca <enriquecoscarelli@gmail.com>
2023-08-16 09:31:23 +0200

    HTTP REQUESTS

--- app.js
+++ app.js
@@ -2,10 +2,34 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    console.log(req.method);
+  res.send('GET REQUEST')
+})
+
+app.post('/', (req, res) => {
+    console.log(req.method);
+  res.send('POST REQUEST')
+})
+
+app.put('/', (req, res) => {
+    console.log(req.method);
+  res.send('PUT REQUEST')
+})
+
+app.delete('/', (req, res) => {
+    console.log(req.method);
+  res.send('DELETE REQUEST')
+})
+
+app.get('/stephan', (req, res) => {
+  res.send('Hello Stephan!')
+})
+
+app.get('/christoph', (req, res) => {
+  res.send('Hello Christoph!')
 })
 
 app.listen(port, () => {
